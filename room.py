b2e80ea65bac5b36fef004acdfa2a34f0fc2c655
Gio <clonecomputers1@gmail.com>
2016-02-25 10:40:06 -0500

    added ability to traverse rooms

--- room.py
+++ room.py
@@ -1,6 +1,5 @@
 import palyer
 
-
 class Room:
   
   def __init__(self, name, key_wrd, mobs = [], childern):
@@ -10,7 +9,9 @@ class Room:
     
       if(self.check_mobs(mobs)):
         raise TypeError("use type Entity for mobs")
+        quit(-1)
       self.mops = mobs
+      self.childern = childern
     
   def check_mobs(self, m):
       if len(mobs) <= 1:
@@ -19,3 +20,16 @@ class Room:
         if not(type(i) is Entity):
           return False
       return True
+      
+  def next_room(self, ms_list, key):
+    
+    is_chlid = False
+    for(i in self.childern):
+      if(i == key):
+        is_chlid = True
+        
+    if(is_chlid):
+      return ms_list[key]
+    else:
+      return -1
+        
